9a996e10718546e985ea7a91aa115d33edb71c78
rameem2003 <rameem2019@gmail.com>
2022-02-21 09:16:28 +0600

    Currency unit added

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # curency_converter_app
+## Developer: Mahmood Hassan Rameem
+## _Studing Diploma Engineering_
+## _Computer Technology_
+
+___
+
+<br>
+
+### My website: rameem2003.github.io/Mysite
+
+<br>
+
+### To browse my web site copy it 
+```
+rameem2003.github.io/Mysite
+```
+
+## Task List
+
+- [x] HTML
+- [x] CSS
+- [x] Bootstrap v4.0
+- [x] JavaScript
+---
+
+# This is me
+![profile](./img/me.jpg)
+## Keep code :)
+
--- app.js
+++ app.js
@@ -29,9 +29,17 @@ function convert(){
         output.value = value_one * 0.043;
     }
 
+    if(curOne == "BDT" && curTwo == "PKR"){
+        output.value = value_one * 2.04;
+    }
+
+    if(curOne == "BDT" && curTwo == "JPN"){
+        output.value = value_one * 1.34;
+    }
 
 
-    // indeian rupee to all
+
+    // indian rupee to all
     if(curOne == "INR" && curTwo == "BDT"){
         output.value = value_one * 1.14;
     }
@@ -42,6 +50,14 @@ function convert(){
 
     if(curOne == "INR" && curTwo == "AED"){
         output.value = value_one * 0.049;
+    }
+
+    if(curOne == "INR" && curTwo == "PKR"){
+        output.value = value_one * 2.36;
+    }
+
+    if(curOne == "INR" && curTwo == "JPN"){
+        output.value = value_one * 1.54;
     }
 
 
@@ -58,6 +74,17 @@ function convert(){
         output.value = value_one * 3.67;
     }
 
+    if(curOne == "USD" && curTwo == "PKR"){
+        output.value = value_one * 175.84;
+    }
+
+    if(curOne == "USD" && curTwo == "JPN"){
+        output.value = value_one * 114.98;
+    }
+
+
+
+
     // arab emirates dirhams to all
     if(curOne == "AED" && curTwo == "BDT"){
         output.value = value_one * 23.40;
@@ -69,6 +96,58 @@ function convert(){
 
     if(curOne == "AED" && curTwo == "USD"){
         output.value = value_one * 0.27;
+    }
+
+    if(curOne == "AED" && curTwo == "PKR"){
+        output.value = value_one * 47.87;
+    }
+
+    if(curOne == "AED" && curTwo == "JPN"){
+        output.value = value_one * 31.30;
+    }
+
+    // pakistan rupee to all
+    if(curOne == "PKR" && curTwo == "BDT"){
+        output.value = value_one * 0.49;
+    }
+    
+    if(curOne == "PKR" && curTwo == "INR"){
+        output.value = value_one * 0.42;
+    }
+
+    if(curOne == "PKR" && curTwo == "USD"){
+        output.value = value_one * 0.0057;
+    }
+
+    if(curOne == "PKR" && curTwo == "AED"){
+        output.value = value_one * 0.021;
+    }
+
+    if(curOne == "PKR" && curTwo == "JPN"){
+        output.value = value_one * 0.65;
+    }
+
+
+
+    // Japan yen to all
+    if(curOne == "JPN" && curTwo == "BDT"){
+        output.value = value_one * 0.75;
+    }
+    
+    if(curOne == "JPN" && curTwo == "INR"){
+        output.value = value_one * 0.65;
+    }
+
+    if(curOne == "JPN" && curTwo == "USD"){
+        output.value = value_one * 0.0087;
+    }
+
+    if(curOne == "JPN" && curTwo == "AED"){
+        output.value = value_one * 0.032;
+    }
+
+    if(curOne == "JPN" && curTwo == "PKR"){
+        output.value = value_one * 1.53;
     }
 
 
